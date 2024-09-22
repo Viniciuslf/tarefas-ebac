8746bf4e5ab9c51c65e23f881ff290523dc7c684
Vinícius <63385115+Viniciuslf@users.noreply.github.com>
2024-09-22 10:39:07 -0300

    md37

---
+++ ProjetoMaven/src/main/java/com/ana/App.java
@@ -0,0 +1,13 @@
+package com.ana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
