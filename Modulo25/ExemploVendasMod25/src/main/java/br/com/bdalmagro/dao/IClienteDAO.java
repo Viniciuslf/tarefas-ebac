6ecade6cddbcf00a5ee46147e5ea9cf443afaf08
Vinícius <63385115+Viniciuslf@users.noreply.github.com>
2025-01-19 08:27:56 -0300

    Update IClienteDAO.java

--- Modulo25/ExemploVendasMod25/src/main/java/br/com/bdalmagro/dao/IClienteDAO.java
+++ Modulo25/ExemploVendasMod25/src/main/java/br/com/bdalmagro/dao/IClienteDAO.java
@@ -6,10 +6,7 @@ package br.com.bdalmagro.dao;
 import br.com.bdalmagro.dao.generic.IGenericDAO;
 import br.com.bdalmagro.domain.Cliente;
 
-/**
- * @author Bruna.D
- *
- */
+
 public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
 
 
